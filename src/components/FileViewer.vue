7d68cb5312c25ccf41f01ea31165f0a8f9f7b39f
DVPervukhin <dvperv@gmail.com>
2021-02-06 12:22:37 +0300

    pre Mongo

--- src/components/FileViewer.vue
+++ src/components/FileViewer.vue
@@ -1,31 +1,26 @@
 <template>
   <section>
     <div class="container has-text-centered">
-      <h2><strong>Pic</strong>
-        <div class="block">
-          <b-radio v-model="checked_pic_class"
-                   name="picture_class"
-                   native-value="yes">
-            <b-icon icon="check-all" type="is-success"></b-icon>
-          </b-radio>
-          <b-radio v-model="checked_pic_class"
-                   name="picture_class"
-                   native-value="no">
-            <b-icon icon="skull-crossbones-outline" type="is-danger"></b-icon>
-          </b-radio>
-          <b-radio v-model="checked_pic_class"
-                   name="picture_class"
-                   native-value="uncertain">
-            <b-icon icon="help-circle-outline" type="is-warning"></b-icon>
-          </b-radio>
-          <b-button icon-left="cancel" rounded>Удалить</b-button>
-        </div>
-        </h2>
+      <h2><strong>Транзиент</strong></h2>
       <div class="columns">
         <div class="column"></div>
         <div class="column is-three-quarters">
           <b-image v-bind:src=picData></b-image>
 <!--          <img src="https://www.torontozoo.com/img/1200/20180605032116763AfricanLion.jpg">-->
+          <div class="notification is-vcentered">
+            <div class="block">
+              <b-radio v-model="radio" size="is-large" name="picture_class" native-value="yes">
+                <b-icon icon="check-all" type="is-success"></b-icon>
+              </b-radio>
+              <b-radio v-model="radio" size="is-large" name="picture_class" native-value="no">
+                <b-icon icon="skull-crossbones-outline" type="is-danger"></b-icon>
+              </b-radio>
+              <b-radio v-model="radio" size="is-large" name="picture_class" native-value="uncertain">
+                <b-icon icon="help-circle-outline" type="is-warning"></b-icon>
+              </b-radio>
+              <b-button icon-left="cancel" rounded>Удалить</b-button>
+            </div>
+          </div>
         </div>
         <div class="column"></div>
       </div>
@@ -35,13 +30,18 @@
 
 <script>
 import axios from "axios";
+import {fileIndex} from "@/services/services";
 
 export default {
   name: "FileViewer",
+  props: {
+    path: String,
+  },
   data() {
     return {
       info: null,
       picData: null,
+      radio: null,
     };
   },
   watch: {
@@ -59,19 +59,22 @@ export default {
       canvas.width = this.info.data.npoints;
       canvas.height = this.info.data.nbands + 1;
       let ctx = canvas.getContext('2d');
+      console.log('info length: ' + this.info.data.grayscale255.length +' for: ' + this.path);
       ctx.putImageData(new ImageData(res, this.info.data.npoints, this.info.data.nbands + 1),0,0);
       this.picData = canvas.toDataURL('image/png');
-
-      console.log('npoints: ', this.info.data.npoints)
-      console.log('nbands + 1: ', this.info.data.nbands + 1)
-      console.log("Grayscale length x4: ", this.info.data.grayscale255.length * 4)
-      console.log("npoints x (nbands + 1): ", this.info.data.npoints * (this.info.data.nbands + 1))
+    },
+    radio(){
+      console.log(this.radio)
+      fileIndex(this.path);
+      //TODO upsert record
     }
   },
   mounted() {
-    axios
-        .get(' https://labelocalapi2.herokuapp.com/file')
-        .then(response => (this.info = response));
+    if(this.path) {
+      axios
+          .post(' https://labelocalapi2.herokuapp.com/file', {path: this.path})
+          .then(response => (this.info = response));
+    }
   }
 }
 </script>
